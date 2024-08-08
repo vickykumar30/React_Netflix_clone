90aeafb13b29006da19e08d8cfaa616c59a45806
vicky <vickyagarawal14300@gmail.com>
2024-08-08 11:04:58 +0530

    title-image push

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 
+
+const cardsRef = useRef();
+
+useEffect(()=>{
+  
+})
 const TitleCards = () => {
   return (
-    <div className='titlecards'>
-      
+    <div className='title-cards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index)=>{
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
